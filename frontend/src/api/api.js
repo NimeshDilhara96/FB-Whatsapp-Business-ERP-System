c052da1b711b7f249cb73871202d1cf5ddb2da32
NimeshDilhara96 <nimeshdilhara2001@gmail.com>
2026-06-25 12:50:46 +0530

    feat: implement axios instance with request and response interceptors for JWT authentication and token refreshing

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import { useAuthStore } from "../store/authStore";
 import { refreshToken } from "../services/authService";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   withCredentials: true, // IMPORTANT: Allows sending/receiving cookies
 });
 
